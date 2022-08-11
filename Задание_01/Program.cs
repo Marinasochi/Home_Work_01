a3a8de1a04b9a7ff6aacb907989b8bdda503c397
marinamyazina <marinasochi1@rambler.ru>
2022-08-11 19:17:12 +0300

    Выполнили первое Задание_01

---
+++ Задание_01/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+// Например: a = 5; b = 7 -> max = 7, min = 5
+
+int Prompt(string message)
+{
+    System.Console.Write(message);    // Вывести сообщение
+    string readValue = Console.ReadLine();   // Считывает с консоли строку
+    int result = int.Parse(readValue);    // Преобразует строку в целое число
+    return result;                           // Возвращает результат функции
+}
+
+int число1 = Prompt($"Введите число1 ");
+int число2 = Prompt($"Введите число2 ");
+if (число1 < число2)
+{
+    Console.WriteLine($"max = {число2}");
+    Console.WriteLine($"min = {число1}");
+}
+if (число1 == число2)
+{
+    Console.WriteLine($"max=min= {число1}");
+}
+if (число1 > число2)
+{
+    Console.WriteLine($"max = {число1}");
+    Console.WriteLine($"min = {число2}");
+}
