7c49cb30b23d2278f21883e659c1bdcadb3c21ca
marinamyazina <marinasochi1@rambler.ru>
2022-08-15 20:57:11 +0300

    Отформатировали коды

--- Задание_02/Program.cs
+++ Задание_02/Program.cs
@@ -1,30 +1,22 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт 
 // максимальное из этих чисел. Пример: 2, 3, 7 -> 7
 
-internal class Program
+  int Prompt(string message)
 {
-    private static void Main(string[] args)
-    {
-        int Prompt(string message)
-        {
-            Console.Write(message);    // Вывести сообщение
-            string readValue = Console.ReadLine();   // Считывает с консоли строку
-            int result = int.Parse(readValue);    // Преобразует строку в целое число
-            return result;                           // Возвращает результат функции
-        }
-
-        int max = Prompt($"Введите число1 ");
-        int число2 = Prompt($"Введите число2 ");
-        int число3 = Prompt($"Введите число3 ");
-        
-        if (число2 > max)
-        {
-            max = число2;
-        }
-        if (число3 > max)
-        {
-            max = число3;
-        }
-        Console.WriteLine($"max = {max}");
-    }
+     Console.Write(message);           // Вывести сообщение
+     string readValue = Console.ReadLine();   // Считывает с консоли строку
+     int result = int.Parse(readValue);    // Преобразует строку в целое число
+     return result;                           // Возвращает результат функции  }
 }
+int max = Prompt($"Введите число1 ");
+int число2 = Prompt($"Введите число2 ");
+int число3 = Prompt($"Введите число3 ");      
+if (число2 > max)
+{
+   max = число2;
+}
+if (число3 > max)
+{
+   max = число3;
+}
+   Console.WriteLine($"max = {max}");
